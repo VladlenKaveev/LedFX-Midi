3511caff4cd3cf81acbbe8bea0ef565c5e289557
VladlenKaveev <67223203+VladlenKaveev@users.noreply.github.com>
2020-08-19 19:27:15 +0300

    Add midi slider. Midi Control works faster

--- ledfx/frontend/components/MidiControl/MidiControl.jsx
+++ ledfx/frontend/components/MidiControl/MidiControl.jsx
@@ -32,171 +32,183 @@ const styles = (theme) => ({
 
 var navigator = require("web-midi-api");
 var navigator = require("jzz");
-var midi;
-var inputs;
-var outputs;
 
 class MidiControl extends React.Component {
+  midi;
+  inputs;
+  ouputs;
+
+  componentDidMount() {
+    navigator
+      .requestMIDIAccess()
+      .then(this.onMIDISuccess.bind(this), this.onMIDIFailure.bind(this));
+  }
+
   constructor(props) {
     super(props);
-    this.state = { status: null, midiname: null };
+    this.state = { status: "null", midiname: "null", brightnessSlider: 0 };
+  }
+
+  stopOutputs() {
+    this.outputs.forEach(function (port) {
+      port.send([0x80, 60, 0]);
+    });
+    this.testInputs();
+  }
+
+  onMIDISuccess(midiAccess) {
+    if (!midiAccess) {
+      debugger;
+    }
+    this.midi = midiAccess;
+    this.inputs = midiAccess.inputs;
+    this.outputs = midiAccess.outputs;
+    setTimeout(this.midiOutput.bind(this), 500);
+  }
+
+  onMIDIFailure(msg) {
+    console.log("Failed to get MIDI access - " + msg);
+    process.exit(1);
+  }
+
+  onMidiIn(ev) {
+    var arr = [];
+    for (var i = 0; i < ev.data.length; i++) {
+      arr.push((ev.data[i] < 16 ? "0" : "") + ev.data[i].toString(16));
+    }
+    console.log("MIDI:", arr.join(" "));
+    if (arr[0] == 97 && arr[1] == 0 && arr[2] != 0) {
+      this.props.activatePreset("1");
+    }
+    if (arr[0] == 97 && arr[1] == 1 && arr[2] != 0) {
+      this.props.activatePreset("2");
+    }
+    if (arr[0] == 97 && arr[1] == 2 && arr[2] != 0) {
+      this.props.activatePreset("3");
+    }
+    if (arr[0] == "b0" && arr[1] == 13 && arr[0] != 0) {
+      console.log("Four");
+      this.setState({ brightnessSlider: parseInt(arr[2], 16) });
+    }
+  }
+
+  testInputs() {
+    console.log("Testing MIDI-In ports...");
+    this.inputs.forEach(
+      function (port) {
+        console.log("id:", port.id);
+        port.onmidimessage = this.onMidiIn.bind(this);
+      }.bind(this)
+    );
+  }
+
+  midiOutput() {
+    console.log("Testing MIDI-Out ports...");
+    this.outputs.forEach(
+      function (port) {
+        console.log(
+          "id:",
+          port.id,
+          "manufacturer:",
+          port.manufacturer,
+          "name:",
+          port.name,
+          "version:",
+          port.version
+        );
+        this.midiname = port.name;
+        port.open();
+        port.send([0x90, 60, 0x7f]);
+      }.bind(this)
+    );
+    setTimeout(this.stopOutputs.bind(this), 1000);
+  }
+
+  startMidi() {
+    if (navigator.requestMIDIAccess) {
+      console.log("WebMIDI is supported in this browser.");
+      this.status = "WebMIDI is supported in this browser.";
+    } else {
+      console.log("WebMIDI is not supported in this browser.");
+      this.status = "WebMIDI is not supported in this browser.";
+    }
   }
 
   updateMidi() {
+    this.startMidi();
     this.setState(() => {
-      return { status: startMidi.status, midiname: midiOutput.midiname };
+      return { status: this.status, midiname: this.midiname };
     });
   }
 
   render() {
     const { activatePreset } = this.props;
-    startMidi();
+    const { brightnessSlider } = this.state;
     return (
       <div>
-            <Typography variant="h5" color="inherit">
-              Midi Control
-            </Typography>
-            <h3>Device </h3>
-            <CardActions>
-              <Button onClick={() => this.updateMidi()}>SET MIDI</Button>
-              <Typography>{this.state.midiname}</Typography>
-              <ul></ul>
-              <Typography>Status: {this.state.status}</Typography>
-            </CardActions>
-            <h3>Presets</h3>
-            <CardActions>
-              <Button
-                id="midibutton"
-                color="primary"
-                size="small"
-                aria-label="Activate"
-                variant="contained"
-                onClick={() => activatePreset("rainbow")}
-              >
-                Rainbow
-              </Button>
-              <Button
-                id="midibutton2"
-                color="primary"
-                size="small"
-                aria-label="Activate"
-                variant="contained"
-                onClick={() => activatePreset("off")}
-              >
-                OFF
-              </Button>
-              <Button
-                id="midibutton3"
-                color="primary"
-                size="small"
-                aria-label="Activate"
-                variant="contained"
-                onClick={() => activatePreset("strobe")}
-              >
-                STROBE
-              </Button>
-              <ul></ul>
-              <Typography id="range-slider" gutterBottom>
-                Brightness
-              </Typography>
-              <ul></ul>
-              <Slider
-                id="midislider"
-                defaultValue={0.5}
-                aria-labelledby="discrete-slider"
-                valueLabelDisplay="auto"
-                step={0.1}
-                marks
-                min={0}
-                max={1}
-              ></Slider>
-            </CardActions>
+        <Typography variant="h5" color="inherit">
+          Midi Control
+        </Typography>
+        <h3>Device </h3>
+        <CardActions>
+          <Button onClick={() => this.updateMidi()}>SET MIDI</Button>
+          <Typography>{this.state.midiname}</Typography>
+          <ul></ul>
+          <Typography>Status: {this.state.status}</Typography>
+        </CardActions>
+        <h3>Presets</h3>
+        <CardActions>
+          <Button
+            id="midibutton"
+            color="primary"
+            size="small"
+            aria-label="Activate"
+            variant="contained"
+            onClick={() => activatePreset("1")}
+          >
+            1
+          </Button>
+          <Button
+            id="midibutton2"
+            color="primary"
+            size="small"
+            aria-label="Activate"
+            variant="contained"
+            onClick={() => activatePreset("2")}
+          >
+            2
+          </Button>
+          <Button
+            id="midibutton3"
+            color="primary"
+            size="small"
+            aria-label="Activate"
+            variant="contained"
+            onClick={() => activatePreset("3")}
+          >
+            3
+          </Button>
+          <ul></ul>
+          <Typography id="range-slider" gutterBottom>
+            Brightness
+          </Typography>
+          <ul></ul>
+          <Slider
+            id="midislider"
+            defaultValue={0}
+            aria-labelledby="discrete-slider"
+            valueLabelDisplay="auto"
+            step={1}
+            marks
+            min={0}
+            max={127}
+            value={brightnessSlider}
+          ></Slider>
+          <div>{brightnessSlider}</div>
+        </CardActions>
       </div>
     );
   }
-}
-
-function startMidi() {
-  if (navigator.requestMIDIAccess) {
-    console.log("WebMIDI is supported in this browser.");
-    startMidi.status = "WebMIDI is supported in this browser.";
-  } else {
-    console.log("WebMIDI is not supported in this browser.");
-    startMidi.status = "WebMIDI is not supported in this browser.";
-  }
-}
-
-function onMIDIFailure(msg) {
-  console.log("Failed to get MIDI access - " + msg);
-  process.exit(1);
-}
-
-function onMIDISuccess(midiAccess) {
-  midi = midiAccess;
-  inputs = midi.inputs;
-  outputs = midi.outputs;
-  setTimeout(midiOutput, 500);
-}
-
-function midiOutput() {
-  console.log("Testing MIDI-Out ports...");
-  outputs.forEach(function (port) {
-    console.log(
-      "id:",
-      port.id,
-      "manufacturer:",
-      port.manufacturer,
-      "name:",
-      port.name,
-      "version:",
-      port.version
-    );
-    midiOutput.midiname = port.name;
-    port.open();
-    port.send([0x90, 60, 0x7f]);
-  });
-  setTimeout(stopOutputs, 1000);
-}
-
-function stopOutputs() {
-  outputs.forEach(function (port) {
-    port.send([0x80, 60, 0]);
-  });
-  testInputs();
-}
-
-function onMidiIn(ev) {
-  var arr = [];
-  for (var i = 0; i < ev.data.length; i++) {
-    arr.push((ev.data[i] < 16 ? "0" : "") + ev.data[i].toString(16));
-  }
-  console.log("MIDI:", arr.join(" "));
-  if (arr[0] == 97 && arr[1] == 0 && arr[2] != 0) {
-    console.log("One");
-    const button = document.querySelector("#midibutton");
-    button.click();
-  }
-  if (arr[0] == 97 && arr[1] == 1 && arr[2] != 0) {
-    console.log("Two");
-    const button = document.querySelector("#midibutton2");
-    button.click();
-  }
-  if (arr[0] == 97 && arr[1] == 2 && arr[2] != 0) {
-    console.log("Three");
-    const button = document.querySelector("#midibutton3");
-    button.click();
-  }
-  if (arr[0] == "b0" && arr[1] == 13 && arr[0] != 0) {
-    console.log("Four");
-  }
-}
-
-function testInputs() {
-  console.log("Testing MIDI-In ports...");
-  inputs.forEach(function (port) {
-    console.log("id:", port.id);
-    port.onmidimessage = onMidiIn;
-  });
 }
 
 MidiControl.propTypes = {
@@ -207,8 +219,6 @@ MidiControl.propTypes = {
 const mapDispatchToProps = (dispatch) => ({
   activatePreset: (presetId) => dispatch(activatePreset(presetId)),
 });
-
-navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
 
 export default connect(
   null,
--- ledfx/frontend/routes/views.jsx
+++ ledfx/frontend/routes/views.jsx
@@ -13,7 +13,6 @@ import PresetsView from "frontend/views/Presets/Presets.jsx";
 import DeviceView from "frontend/views/Device/Device.jsx";
 import SettingsView from "frontend/views/Settings/Settings.jsx";
 import DeveloperView from "frontend/views/Developer/Developer.jsx";
-import MidiView from "frontend/views/Midi/Midi.jsx"
 
 const viewRoutes = [
   {
